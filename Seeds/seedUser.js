158615edb978bd9a63d767e2f8ad63507ccf40a6
Abi L <124858748+lailiel@users.noreply.github.com>
2023-07-05 18:40:30 -0500

    added temp styles, built out seeds, built out api controllers, built out models for post and index, updated schema, updated index files. updated server.js

---
+++ Seeds/seedUser.js
@@ -0,0 +1,25 @@
+const { User } = require ("../models")
+
+ const userData = [
+    {
+      name: "bob83", 
+      email: "bobsemail@email.com",
+      password: "bobspw",
+    },
+    {
+        name: "sarah45", 
+        email: "sarahsemail@email.com",
+        password: "sarahspw",
+    },
+    {
+        name: "phil45", 
+        email: "philsemail@email.com",
+        password: "philspw",
+    }, 
+ ]
+
+ const seedUser = async () => {
+    await User.bulkCreate(userData)
+ }
+
+ module.exports = seedUser;
---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require ("./user");
+const Post = require ("./post");
+
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+User.hasMany(Post, {
+    foreignKey: "user_id"
+})
+
+module.exports = {User, Post}
